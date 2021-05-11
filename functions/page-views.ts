de2aefefa6498054eb99c959501c97f9e43577dd
KingRayhan <rayhan095@gmail.com>
2021-05-12 04:45:06 +0600

    cors support

--- functions/page-views.ts
+++ functions/page-views.ts
@@ -46,6 +46,9 @@ export const handler: Handler = async (
   const res = await axios.get(api, {
     headers: {
       Authorization: "Bearer " + process.env.API_TOKEN,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
     },
   });
 
